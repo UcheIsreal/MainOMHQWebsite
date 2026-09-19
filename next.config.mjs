d140ae872e1b30de722531aa3b8bccb619c2429f
UcheIsreal <147603233+UcheIsreal@users.noreply.github.com>
2026-09-19 20:21:46 +0100

    Remove remotePatterns from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "https://www.image2url.com/",
-      },
-    ],
-  },
+  
 };
 
 export default nextConfig;
