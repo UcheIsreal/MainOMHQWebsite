6cd24804ea21c14cc8b383a208b69608c49f0e7e
UcheIsreal <147603233+UcheIsreal@users.noreply.github.com>
2026-09-19 20:56:27 +0100

    Fix logo image source path in Logo component

--- components/Logo.js
+++ components/Logo.js
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2.5 shrink-0">
       <Image
-        src="\public\logo.png"
+        src="/logo.png"
         alt="Online Marketing HQ Logo"
         width={20}
         height={20}
