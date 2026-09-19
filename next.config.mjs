703f2cc2d4353d301a3cdaa8b1bd9441f5caf006
UcheIsreal <147603233+UcheIsreal@users.noreply.github.com>
2026-09-19 20:20:48 +0100

    Add remote patterns for image hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://www.image2url.com/",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
